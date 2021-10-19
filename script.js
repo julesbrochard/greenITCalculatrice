c2fd6256e5f0d7085c7c3309131ac08c199ad856
Dion <sofiane.dion@laposte.net>
2021-10-19 21:07:57 +0200

    2ème avancée HTML
    Ajout des dernières lignes de codes HTML
    Ajout du Cloud
    Ajout Impressions
    Ajout Trajet maison entreprise
    Ajout Trajets professionnels
    Ajout Bouton Calculer

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function function_YoN_computer() {
+function function_YoN_ordinateur() {
     // Récupère l'id si le input est coché
-    var checkBox = document.getElementById("checkComputer");
+    var checkBox = document.getElementById("verifOrdinateur");
     // Récupère l'id à affiché
-    var text = document.getElementById("equipment_computer");
+    var text = document.getElementById("equipement_ordinateur");
 
     // Si la case est coché, affiche le formulaire de l'ordinateur
     if (checkBox.checked == true) {
@@ -12,11 +12,25 @@ function function_YoN_computer() {
     }
 }
 
-function function_YoN_phone() {
+function function_YoN_telephone() {
     // Récupère l'id si le input est coché
-    var checkBox = document.getElementById("checkPhone");
+    var checkBox = document.getElementById("verifTelephone");
     // Récupère l'id à affiché
-    var text = document.getElementById("equipment_phone");
+    var text = document.getElementById("equipement_telephone");
+
+    // Si la case est coché, affiche le formulaire du portable
+    if (checkBox.checked == true) {
+        text.style.display = "block";
+    } else {
+        text.style.display = "none";
+    }
+}
+
+function function_YoN_covoiturage() {
+    // Récupère l'id si le input est coché
+    var checkBox = document.getElementById("verifCovoiturage");
+    // Récupère l'id à affiché
+    var text = document.getElementById("trajet_covoiturage");
 
     // Si la case est coché, affiche le formulaire du portable
     if (checkBox.checked == true) {
